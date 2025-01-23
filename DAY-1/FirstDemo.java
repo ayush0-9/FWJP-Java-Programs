396558f04fdcac17091db117f94d601fd72352c2
AYUSH JOSHI <153922853+ayush0-9@users.noreply.github.com>
2025-01-23 16:24:03 +0530

    Add files via upload

---
+++ DAY-1/FirstDemo.java
@@ -0,0 +1,13 @@
+class FirstDemo
+{
+ public static void main(String args[])
+ {
+ // single line comments
+ /* 
+ multiline comments
+ */
+ // print a message on console screen
+ 
+ system.out.println("Hello World");
+ }
+}
