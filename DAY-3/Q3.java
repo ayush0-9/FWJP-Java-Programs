b32bc44d92b3ecdccc15dfedde44d485683d8e58
AYUSH JOSHI <153922853+ayush0-9@users.noreply.github.com>
2025-01-23 16:16:10 +0530

    Add files via upload

---
+++ DAY-3/Q3.java
@@ -0,0 +1,12 @@
+class Percentage {
+    public static void main(String[] args) {
+        
+        int totalMarks = 500;
+        int obtainedMarks = 430;
+
+        double percentage = (double) obtainedMarks / totalMarks * 100;
+
+        System.out.println("The student scored " + obtainedMarks + " marks out of " + totalMarks);
+        System.out.println("The percentage is: " + percentage + "%");
+    }
+}
